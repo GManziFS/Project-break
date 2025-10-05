2a666d43f54d2cab2e32095d5e07c16e682bdfa6
GManziFS <127343298+GManziFS@users.noreply.github.com>
2025-10-04 21:56:51 -0300

    third commit
    Everything works; we’re looking to transfer it to NASA’s database

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let map;
 let marker;
 let geocoder;
 let autocomplete;
+let latitude;
+let longitude;
+let date;
+let response;
 
 function initMap() {
   const initialPosition = { lat: 39.704965, lng: -101.680907 };
@@ -76,7 +80,7 @@ function placeMarker(location) {
       draggable: true,
     });
     marker.addListener("dragend", () => {
-      pos = marker.getPosition();
+      const pos = marker.getPosition();
       updateCoords(pos.lat(), pos.lng());
     });
   }
@@ -86,7 +90,12 @@ function placeMarker(location) {
 }
 
 function updateCoords(lat, lng) {
+  latitude = lat;
+  longitude = lng;
+
   document.getElementById("coords").innerText = "Latitude: " + lat.toFixed(6) + ", Longitude: " + lng.toFixed(6);
+
+  console.log("Latitude:", latitude, "Longitude:", longitude);
 }
 
 function searchAddress(address) {
@@ -102,23 +111,62 @@ function searchAddress(address) {
   });
 }
 
-maxTemp = document.getElementById("max-temp")
-minTemp = document.getElementById("min-temp")
-btnTemp = document.getElementById("btn-temp")
+const maxTemp = document.getElementById("max-temp");
+const minTemp = document.getElementById("min-temp");
+const btnTemp = document.getElementById("btn-temp");
+const iptDate = document.getElementById("dateInput");
+
+iptDate.addEventListener("change", () => {
+  
+})
+
+
 
 btnTemp.addEventListener("click", async () => {
+  date = iptDate.value
+  if(!date){
+    alert("Please select a date.")
+    return
+  }else{
+    maxTemp.innerText = ""
+    minTemp.innerText = "Carregando..."
+  }
+
+  const currentDate = new Date();
+  currentDate.setHours(0,0,0,0);
+
+  const userDate = new Date(date);
+  userDate.setHours(0,0,0,0);
+
+  console.log("Hoje: ",currentDate)
+  console.log("antes: ",userDate)
+  
+  const pastApi = `http://api.weatherapi.com/v1/history.json?key=741cb8159a274a2f952144447250410&q=${latitude},${longitude}&dt=${date}`
+  const currentApi = `http://api.weatherapi.com/v1/forecast.json?key=741cb8159a274a2f952144447250410&q=${latitude},${longitude}&days=14&aqi=yes&alerts=yes`
+  const futureApi = `http://api.weatherapi.com/v1/future.json?key=741cb8159a274a2f952144447250410&q=${latitude},${longitude}&dt=${date}`
+
+  const mlsecInDay = 24 * 60 * 60 * 1000;
+  const futureLimit = 14 * mlsecInDay;
+
   try {
-    const response = await fetch("http://api.weatherapi.com/v1/future.json?key=741cb8159a274a2f952144447250410&q=56.00,-90.00&dt=2026-06-18")
-    if(!response.ok) throw new Error("Erro na requisição: "+response.status)
-    const data = await response.json()
-    console.log("Dados: ", data)
+    if(userDate.getTime() == currentDate.getTime() || (userDate.getTime() > currentDate.getTime() && userDate.getTime() < currentDate.getTime() + futureLimit)){
+      response = await fetch(currentApi);
+    } else if (userDate.getTime() < currentDate.getTime()){
+      response = await fetch(pastApi)
+    } else {
+      response = await fetch(futureApi)
+    }
+    if (!response.ok) throw new Error("Erro na requisição: " + response.status);
+    const data = await response.json();
+    console.log("Dados: ", data);
+    console.log(response)
 
-    const forecastDay = data.forecast.forecastday[0].day
+    const forecastDay = data.forecast.forecastday[0].day;
 
-    minTemp.innerText = `Temperatura minima: ${forecastDay.mintemp_c} °C`
-    maxTemp.innerText = `Temperatura máxima: ${forecastDay.maxtemp_c} °C`
+    minTemp.innerText = `Temperatura minima: ${forecastDay.mintemp_c} °C`;
+    maxTemp.innerText = `Temperatura máxima: ${forecastDay.maxtemp_c} °C`;
 
   } catch (error) {
-    console.error("Erro: ",error);
+    console.error("Erro: ", error);
   }
-})
+});
