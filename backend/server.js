a453ca7e5fec62321d06e67d99dce07373d9e3aa
GManziFS <127343298+GManziFS@users.noreply.github.com>
2025-10-05 22:13:32 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'https://project-break-x1kk.onrender.com',
-    'https://project-break-two.vercel.app/'];
+    'https://project-break-two.vercel.app'];
 
 app.use(cors({
     origin: function (origin, callback) {
